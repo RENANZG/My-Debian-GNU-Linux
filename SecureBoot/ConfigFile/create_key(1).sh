0b1378cfdac5b26c92e53cde5de69a775e095f5b
Renan Zamboni Gomes <53377291+RENANZG@users.noreply.github.com>
2023-09-26 15:09:29 -0300

    Add files via upload

---
+++ SecureBoot/ConfigFile/create_key(1).sh
@@ -0,0 +1,21 @@
+### UNDER WORK ###
+
+cd /lib/modules/$(uname -r)/build/certs
+
+sudo tee x509.genkey > /dev/null << 'EOF'
+[ req ]
+default_bits = 4096
+distinguished_name = req_distinguished_name
+prompt = no
+string_mask = utf8only
+x509_extensions = myexts
+[ req_distinguished_name ]
+CN = Modules
+[ myexts ]
+basicConstraints=critical,CA:FALSE
+keyUsage=digitalSignature
+subjectKeyIdentifier=hash
+authorityKeyIdentifier=keyid
+EOF
+
+sudo openssl req -new -nodes -utf8 -sha512 -days 36500 -batch -x509 -config x509.genkey -outform DER -out signing_key.x509 -keyout signing_key.pem
